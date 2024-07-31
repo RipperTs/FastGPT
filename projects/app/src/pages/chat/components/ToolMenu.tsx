31e6799859458cdff6cb38e6253a4c0904ce70c4
ripper <wangyifani@foxmail.com>
2024-07-31 11:08:25 +0800

    ✨ 增加了基于 Cookies 的退出登录功能，并完善了导出选项，包括 HTML 和 PDF 格式.

--- projects/app/src/pages/chat/components/ToolMenu.tsx
+++ projects/app/src/pages/chat/components/ToolMenu.tsx
@@ -6,11 +6,66 @@ import { useTranslation } from 'next-i18next';
 import MyIcon from '@fastgpt/web/components/common/Icon';
 import { useRouter } from 'next/router';
 import MyMenu from '@fastgpt/web/components/common/MyMenu';
+import Cookies from 'js-cookie';
 
 const ToolMenu = ({ history }: { history: ChatItemType[] }) => {
   const { t } = useTranslation();
   const { onExportChat } = useChatBox();
   const router = useRouter();
+  const cardNo = Cookies.get('card_no') || '';
+
+  const menuList = [
+    {
+      children: [
+        {
+          icon: 'core/chat/chatLight',
+          label: t('common:core.chat.New Chat'),
+          onClick: () => {
+            router.replace({
+              query: {
+                ...router.query,
+                chatId: ''
+              }
+            });
+          }
+        }
+      ]
+    },
+    {
+      children: [
+        {
+          icon: 'core/app/appApiLight',
+          label: `HTML ${t('common:Export')}`,
+          onClick: () => onExportChat({ type: 'html', history })
+        },
+        {
+          icon: 'file/markdown',
+          label: `Markdown ${t('common:Export')}`,
+          onClick: () => onExportChat({ type: 'md', history })
+        },
+        {
+          icon: 'core/chat/export/pdf',
+          label: `PDF ${t('common:Export')}`,
+          onClick: () => onExportChat({ type: 'pdf', history })
+        }
+      ]
+    }
+  ];
+
+  if (cardNo) {
+    menuList.push({
+      children: [
+        {
+          icon: 'out',
+          label: '退出登录',
+          onClick: () => {
+            Cookies.remove('card_no');
+            window.location.reload();
+          }
+        }
+      ]
+    });
+  }
 
   return history.length > 0 ? (
     <MyMenu
@@ -22,43 +77,7 @@ const ToolMenu = ({ history }: { history: ChatItemType[] }) => {
           variant={'whitePrimary'}
         />
       }
-      menuList={[
-        {
-          children: [
-            {
-              icon: 'core/chat/chatLight',
-              label: t('common:core.chat.New Chat'),
-              onClick: () => {
-                router.replace({
-                  query: {
-                    ...router.query,
-                    chatId: ''
-                  }
-                });
-              }
-            }
-          ]
-        },
-        {
-          children: [
-            // {
-            //   icon: 'core/app/appApiLight',
-            //   label: `HTML ${t('common:Export')}`,
-            //   onClick: () => onExportChat({ type: 'html', history })
-            // },
-            {
-              icon: 'file/markdown',
-              label: `Markdown ${t('common:Export')}`,
-              onClick: () => onExportChat({ type: 'md', history })
-            }
-            // {
-            //   icon: 'core/chat/export/pdf',
-            //   label: `PDF ${t('common:Export')}`,
-            //   onClick: () => onExportChat({ type: 'pdf', history })
-            // }
-          ]
-        }
-      ]}
+      menuList={menuList}
     />
   ) : (
     <Box w={'28px'} h={'28px'} />
