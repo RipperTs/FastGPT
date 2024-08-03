89bf2ac0ca64276a75f23bcd32a3a25e4c51c144
ripper <wangyifani@foxmail.com>
2024-08-03 09:24:41 +0800

    ✨ 添加新的 delHistory.ts 文件以实现删除历史记录功能

---
+++ projects/app/src/pages/api/core/chat/openApi/delHistory.ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { jsonRes } from '@fastgpt/service/common/response';
+import { MongoChat } from '@fastgpt/service/core/chat/chatSchema';
+import { MongoChatItem } from '@fastgpt/service/core/chat/chatItemSchema';
+import { DelHistoryProps } from '@/global/core/chat/api';
+import { authChatCrud } from '@/service/support/permission/auth/chat';
+import { mongoSessionRun } from '@fastgpt/service/common/mongo/sessionRun';
+import { NextAPI } from '@/service/middleware/entry';
+import { ApiRequestProps } from '@fastgpt/service/type/next';
+import {ReadPermissionVal, WritePermissionVal} from '@fastgpt/global/support/permission/constant';
+import {parseHeaderCert} from "@fastgpt/service/support/permission/controller";
+
+/* 删除一条历史记录 */
+async function handler(req: ApiRequestProps<{}, DelHistoryProps>, res: NextApiResponse) {
+  const { chatId } = req.query;
+
+  if (!chatId){
+    return jsonRes(res, {
+      code: 422,
+      message: 'chatId is required'
+    });
+  }
+
+  // 从header头中获取身份凭证信息
+  const {appId} = await parseHeaderCert({
+    req,
+    authApiKey: true,
+    per: ReadPermissionVal
+  });
+
+  await mongoSessionRun(async (session) => {
+    await MongoChatItem.deleteMany(
+      {
+        appId,
+        chatId
+      },
+      { session }
+    );
+    await MongoChat.findOneAndRemove(
+      {
+        appId,
+        chatId
+      },
+      { session }
+    );
+  });
+
+  jsonRes(res);
+}
+
+export default NextAPI(handler);
